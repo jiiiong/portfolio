aabb42ab4a6b05b1af9ae88bac15f84ffe9b33a0
jiiiong <z.991129@outlook.com>
2024-06-25 17:27:16 +0800

    1. navigator tabs 2. poor home page

---
+++ app.js
@@ -0,0 +1,27 @@
+const navBtns = document.querySelectorAll('.control');
+const allSecs = document.querySelectorAll('.section');
+
+function PageTransitions() {
+    // Button click active class
+    for (const btn of navBtns) {
+        btn.addEventListener("click", (e) => {
+            const currentBtn = document.querySelector('.actived-btn');
+            currentBtn.className = currentBtn.className.replace("actived-btn", "");
+            e.currentTarget.className += " actived-btn";
+            // console.log(`${e.target.dataset.id}`);
+
+            const id = e.currentTarget.dataset.id;
+            for (const sec of allSecs) {
+                sec.classList.remove('active');
+                if (sec.id === id) {
+                    sec.classList.add('active');
+                }
+            }
+            
+        })
+    }
+
+
+}
+
+PageTransitions();
